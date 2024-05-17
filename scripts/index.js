6799df3c1a538e14126378d322a7ce33ce68d69c
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-17 14:16:03 -0500

    finished table exercise by myself

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,39 @@
 
 window.onload = () => {
 
-    
+    loadPeopleIntoTable();
+
+}
+
+function loadPeopleIntoTable(){
+
+    let peopleTableBody = document.querySelector("#peopleTableBody")
+
+    people.forEach((people) => {
+
+        buildTableRow(peopleTableBody, people );
+
+    })
+
+}
+
+function buildTableRow(tbody, data){
+
+    let newRow = tbody.insertRow(-1);
+
+    let td1 = newRow.insertCell(0);
+    td1.innerText = data.id;
+
+    let td2 = newRow.insertCell(1);
+    td2.innerText =data.firstName;
+
+    let td3 = newRow.insertCell(2);
+    td3.innerText =data.lastName;
+
+    let td4 = newRow.insertCell(3);
+    td4.innerText =data.email;
+
+    let td5 = newRow.insertCell(4);
+    td5.innerText =data.ipAddress;
 
 }
